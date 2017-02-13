cdd77af01e1de6bc54bdbe53b5de1b2aa78dbd3a
yupeng <18710095921@163.com>
2017-02-13 17:36:22 +0800

    计算器样式调整

--- unpackage/src/view/bizcomponent/Calculator.vue
+++ unpackage/src/view/bizcomponent/Calculator.vue
@@ -1,7 +1,65 @@
 <template>
 	<div class="mui-scroll-wrapper calculator">
 		<div class="mui-scroll">
-			<div class="oneCalculator">
+			<ul id="list" class="mui-table-view mui-table-view-chevron">
+				<li class="mui-table-view-cell">
+					<a class="mui-navigate-right" href="javascript:void(0)" @tap="steelPlateActive = true">
+						钢板
+					</a>
+				</li>
+				<li class="mui-table-view-cell">
+					<a class="mui-navigate-right" href="javascript:void(0)" @tap="steelPipeActive = true">
+						钢管
+					</a>
+				</li>
+				<li class="mui-table-view-cell">
+					<a class="mui-navigate-right" href="javascript:void(0)" @tap="roundSteelActive = true">
+						圆钢
+					</a>
+				</li>
+				<li class="mui-table-view-cell">
+					<a class="mui-navigate-right" href="javascript:void(0)" @tap="squareSteelActive = true">
+						方钢
+					</a>
+				</li>
+				<li class="mui-table-view-cell">
+					<a class="mui-navigate-right" href="javascript:void(0)" @tap="flatSteelActive = true">
+						扁钢
+					</a>
+				</li>
+				<li class="mui-table-view-cell">
+					<a class="mui-navigate-right" href="javascript:void(0)" @tap="sixAngleSteelActive = true">
+						六角钢
+					</a>
+				</li>
+				<li class="mui-table-view-cell">
+					<a class="mui-navigate-right" href="javascript:void(0)" @tap="screwThreadSteelActive = true">
+						螺纹钢
+					</a>
+				</li>
+				<li class="mui-table-view-cell">
+					<a class="mui-navigate-right" href="javascript:void(0)" @tap="sidePassActive = true">
+						方通
+					</a>
+				</li>
+				<li class="mui-table-view-cell">
+					<a class="mui-navigate-right" href="javascript:void(0)" @tap="flatPassActive = true">
+						扁通
+					</a>
+				</li>
+				<li class="mui-table-view-cell">
+					<a class="mui-navigate-right" href="javascript:void(0)" @tap="equalSideAngleSteelActive = true">
+						等边角钢
+					</a>
+				</li>
+				<li class="mui-table-view-cell">
+					<a class="mui-navigate-right" href="javascript:void(0)" @tap="notEqualSideAngleSteelActive = true">
+						不等边角钢
+					</a>
+				</li>
+			</ul>
+			<div class="oneCalculator {{steelPlateActive ? 'active' : ''}}">
+				<a href="javascript:void(0)" @tap="steelPlateActive = false" class="jxddicon icon-houtui44">返回</a>
 				<img src="../../static/img/calculator/gangban.jpg" />
 				<div class="calculateArea">
 					<p>钢板重量计算</p>
@@ -11,7 +69,8 @@
 					<p><span>重量：</span><input type="text" value="{{steelPlateWeight | weight}} " readonly><span>kg</span></p>
 				</div>
 			</div>
-			<div class="oneCalculator">
+			<div class="oneCalculator {{steelPipeActive ? 'active' : ''}}">
+				<a href="javascript:void(0)" @tap="steelPipeActive = false" class="jxddicon icon-houtui44">返回</a>
 				<img src="../../static/img/calculator/gangguan.jpg" />
 				<div class="calculateArea">
 					<p>钢管重量计算</p>
@@ -21,7 +80,8 @@
 					<p><span>重量：</span><input type="text" value="{{steelPipeWeight | weight}} " readonly><span>kg</span></p>
 				</div>
 			</div>
-			<div class="oneCalculator">
+			<div class="oneCalculator {{roundSteelActive ? 'active' : ''}}">
+				<a href="javascript:void(0)" @tap="roundSteelActive = false" class="jxddicon icon-houtui44">返回</a>
 				<img src="../../static/img/calculator/yuangang.jpg" />
 				<div class="calculateArea">
 					<p>圆钢重量计算</p>
@@ -30,7 +90,8 @@
 					<p><span>重量：</span><input type="text" value="{{roundSteelWeight | weight}} " readonly><span>kg</span></p>
 				</div>
 			</div>
-			<div class="oneCalculator">
+			<div class="oneCalculator {{squareSteelActive ? 'active' : ''}}">
+				<a href="javascript:void(0)" @tap="squareSteelActive = false" class="jxddicon icon-houtui44">返回</a>
 				<img src="../../static/img/calculator/fanggang.jpg" />
 				<div class="calculateArea">
 					<p>方钢重量计算</p>
@@ -39,7 +100,8 @@
 					<p><span>重量：</span><input type="text" value="{{squareSteelWeight | weight}} " readonly><span>kg</span></p>
 				</div>
 			</div>
-			<div class="oneCalculator">
+			<div class="oneCalculator {{flatSteelActive ? 'active' : ''}}">
+				<a href="javascript:void(0)" @tap="flatSteelActive = false" class="jxddicon icon-houtui44">返回</a>
 				<img src="../../static/img/calculator/biangang.jpg" />
 				<div class="calculateArea">
 					<p>扁钢重量计算</p>
@@ -49,7 +111,8 @@
 					<p><span>重量：</span><input type="text" value="{{flatSteelWeight | weight}} " readonly><span>kg</span></p>
 				</div>
 			</div>
-			<div class="oneCalculator">
+			<div class="oneCalculator {{sixAngleSteelActive ? 'active' : ''}}">
+				<a href="javascript:void(0)" @tap="sixAngleSteelActive = false" class="jxddicon icon-houtui44">返回</a>
 				<img src="../../static/img/calculator/liujiaogang.jpg" />
 				<div class="calculateArea">
 					<p>六角钢重量计算</p>
@@ -58,7 +121,8 @@
 					<p><span>重量：</span><input type="text" value="{{sixAngleSteelWeight | weight}} " readonly><span>kg</span></p>
 				</div>
 			</div>
-			<div class="oneCalculator">
+			<div class="oneCalculator {{screwThreadSteelActive ? 'active' : ''}}">
+				<a href="javascript:void(0)" @tap="screwThreadSteelActive = false" class="jxddicon icon-houtui44">返回</a>
 				<img src="../../static/img/calculator/luowengang.jpg" />
 				<div class="calculateArea">
 					<p>螺纹钢重量计算</p>
@@ -67,7 +131,8 @@
 					<p><span>重量：</span><input type="text" value="{{screwThreadSteelWeight | weight}} " readonly><span>kg</span></p>
 				</div>
 			</div>
-			<div class="oneCalculator">
+			<div class="oneCalculator {{sidePassActive ? 'active' : ''}}">
+				<a href="javascript:void(0)" @tap="sidePassActive = false" class="jxddicon icon-houtui44">返回</a>
 				<img src="../../static/img/calculator/fangtong.jpg" />
 				<div class="calculateArea">
 					<p>方通重量计算</p>
@@ -77,7 +142,8 @@
 					<p><span>重量：</span><input type="text" value="{{sidePassWeight | weight}} " readonly><span>kg</span></p>
 				</div>
 			</div>
-			<div class="oneCalculator">
+			<div class="oneCalculator {{flatPassActive ? 'active' : ''}}">
+				<a href="javascript:void(0)" @tap="flatPassActive = false" class="jxddicon icon-houtui44">返回</a>
 				<img src="../../static/img/calculator/biantong.jpg" />
 				<div class="calculateArea">
 					<p>扁通重量计算</p>
@@ -88,7 +154,8 @@
 					<p><span>重量：</span><input type="text" value="{{flatPassWeight | weight}} " readonly><span>kg</span></p>
 				</div>
 			</div>
-			<div class="oneCalculator">
+			<div class="oneCalculator {{equalSideAngleSteelActive ? 'active' : ''}}">
+				<a href="javascript:void(0)" @tap="equalSideAngleSteelActive = false" class="jxddicon icon-houtui44">返回</a>
 				<img src="../../static/img/calculator/dengbianjiaogang.jpg" />
 				<div class="calculateArea">
 					<p>等边角钢重量计算</p>
@@ -98,7 +165,8 @@
 					<p><span>重量：</span><input type="text" value="{{equalSideAngleSteelWeight | weight}} " readonly><span>kg</span></p>
 				</div>
 			</div>
-			<div class="oneCalculator">
+			<div class="oneCalculator {{notEqualSideAngleSteelActive ? 'active' : ''}}">
+			<a href="javascript:void(0)" @tap="notEqualSideAngleSteelActive = false" class="jxddicon icon-houtui44">返回</a>
 				<img src="../../static/img/calculator/budengbianjiaogang.jpg" />
 				<div class="calculateArea">
 					<p>不等边角钢重量计算</p>
@@ -119,43 +187,54 @@
 		data: function() {
 			return {
 				scroll: undefined,
+				steelPlateActive: false,
 				steelPlateLength: undefined,
 				steelPlateWidth: undefined,
 				steelPlateThickness: undefined,
 				steelPlateWeight: 0,
+				steelPipeActive: false,
 				steelPipeLength: undefined,
 				steelPipeDiameter: undefined,
 				steelPipeThickness: undefined,
 				steelPipeWeight: 0,
+				roundSteelActive: false,
 				roundSteelLength: undefined,
 				roundSteelDiameter: undefined,
 				roundSteelWeight: 0,
+				squareSteelActive: false,
 				squareSteelLength: undefined,
 				squareSteelWidth: undefined,
 				squareSteelWeight: 0,
+				flatSteelActive: false,
 				flatSteelLength: undefined,
 				flatSteelWidth: undefined,
 				flatSteelThickness: undefined,
 				flatSteelWeight: 0,
+				sixAngleSteelActive: false,
 				sixAngleSteelLength: undefined,
 				sixAngleSteelDiameter: undefined,
 				sixAngleSteelWeight: 0,
+				screwThreadSteelActive: false,
 				screwThreadSteelLength: undefined,
 				screwThreadSteelDiameter: undefined,
 				screwThreadSteelWeight: 0,
+				sidePassActive: false,
 				sidePassLength: undefined,
 				sidePassWidth: undefined,
 				sidePassThickness: undefined,
 				sidePassWeight: 0,
+				flatPassActive: false,
 				flatPassLength: undefined,
 				flatPassSideLength: undefined,
 				flatPassSideWidth: undefined,
 				flatPassThickness: undefined,
 				flatPassWeight: 0,
+				equalSideAngleSteelActive: false,
 				equalSideAngleSteelLength: undefined,
 				equalSideAngleSteelWidth: undefined,
 				equalSideAngleSteelThickness: undefined,
 				equalSideAngleSteelWeight: 0,
+				notEqualSideAngleSteelActive: false,
 				notEqualSideAngleSteelLength: undefined,
 				notEqualSideAngleSteelWidth1: undefined,
 				notEqualSideAngleSteelWidth2: undefined,
@@ -168,7 +247,7 @@
 				return 785 * this.steelPlateLength * this.steelPlateWidth * this.steelPlateThickness / 100;
 			},
 			steelPipeWeight() {
-				return (this.steelPipeDiameter - this.steelPipeThickness) * this.steelPipeThickness * this.steelPipeLength * 2466 / 100000;
+				return(this.steelPipeDiameter - this.steelPipeThickness) * this.steelPipeThickness * this.steelPipeLength * 2466 / 100000;
 			},
 			roundSteelWeight() {
 				return this.roundSteelDiameter * this.roundSteelDiameter * this.roundSteelLength * 617 / 100000;
@@ -189,13 +268,13 @@
 				return this.sidePassLength * this.sidePassWidth * this.sidePassThickness * 4 * 785 / 100000;
 			},
 			flatPassWeight() {
-				return (this.flatPassSideLength + this.flatPassSideWidth) * 2 * this.flatPassThickness * this.flatPassLength * 785 / 100000;
+				return(this.flatPassSideLength + this.flatPassSideWidth) * 2 * this.flatPassThickness * this.flatPassLength * 785 / 100000;
 			},
 			equalSideAngleSteelWeight() {
 				return this.equalSideAngleSteelLength * this.equalSideAngleSteelWidth * this.equalSideAngleSteelThickness * 15 / 1000;
 			},
 			notEqualSideAngleSteelWeight() {
-				return (this.notEqualSideAngleSteelWidth1 + this.notEqualSideAngleSteelWidth2) * this.notEqualSideAngleSteelLength * this.notEqualSideAngleSteelThickness * 76 / 10000;
+				return(this.notEqualSideAngleSteelWidth1 + this.notEqualSideAngleSteelWidth2) * this.notEqualSideAngleSteelLength * this.notEqualSideAngleSteelThickness * 76 / 10000;
 			}
 		},
 		filters: {
@@ -225,12 +304,38 @@
 		top: 45px;
 		bottom: 50px;
 		width: 100%;
+		color: #003a92;
+	}
+	
+	.mui-scroll {
+		min-height: 100%;
 	}
 	
 	.oneCalculator {
-		padding: 10px;
+		padding: 60px 10px 0 25px;
 		background-color: #fff;
-		margin: 8px 0;
+		transition: transform 500ms;
+		opacity: 0;
+		transform: scale(0);
+		position: absolute;
+		width: 100%;
+		top: 0;
+		bottom: 0;
+	}
+	
+	.oneCalculator > a {
+		position: absolute;
+		top: 0;
+		left: 10px;
+		color: #444;
+		font-size: 15px;
+		line-height: 50px;
+	}
+	
+	.oneCalculator.active {
+		opacity: 1;
+		transform: scale(1);
+		z-index: 99;
 	}
 	
 	.oneCalculator img {
@@ -239,21 +344,19 @@
 		float: left;
 	}
 	
-	.oneCalculator .calculateArea {
+	
+	.oneCalculator .calculateArea p {
+		line-height: 50px;
+		color: #666;
 		padding-left: 50px;
 	}
 	
-	.oneCalculator .calculateArea p {
-		line-height: 30px;
-		color: #666;
-	}
-	
 	.oneCalculator .calculateArea p:nth-child(1) {
-		line-height: 1;
+		line-height: 35px;
 		border-bottom: solid 1px #D7D7D7;
 		color: #333;
 		font-size: 15px;
-		padding-bottom: 10px;
+		padding-bottom: 15px;
 	}
 	
 	.oneCalculator input[type=text] {
@@ -266,5 +369,4 @@
 		margin-bottom: 0;
 		padding: 0;
 	}
-	
 </style>
